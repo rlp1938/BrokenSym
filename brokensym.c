a68c10ebc7c787fd445ed0daba64cfe307cda08f
bob <rlp1938@gmail.com>
2015-09-30 15:23:53 +0700

    Missplaced break; statement fixed.

--- brokensym.c
+++ brokensym.c
@@ -1,6 +1,6 @@
 /*      brokensym.c
  *
- *	Copyright 2011 Bob Parker <rlp1938@gmail.com>
+ *	Copyright 2015 Bob Parker <rlp1938@gmail.com>
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -174,8 +174,8 @@ void recursedir(char *headdir)
 					perror(newdir);
 					break;
 				}
-			break;
 			}
+			break;
 			case DT_DIR:
 			// recurse using this pathname.
 			strcpy(newdir, headdir);
